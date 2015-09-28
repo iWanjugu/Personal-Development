4c1fb86e29f65c6e77a9932b5cd06fd2763efa14
Wanjugu Irene <ireneemaina@gmail.com>
2015-09-28 17:41:57 +0300

    Parenthesis Matcher Complete!

--- paren_matcher/paren_matcher.py
+++ paren_matcher/paren_matcher.py
@@ -5,3 +5,14 @@
 # Not balanced:
 #   '((()'
 #   '())('
+
+def paren_matcher (str):
+    print ("Number of right Parentesis: ", str.count("("))
+    print ("Number of right Parentesis: ", str.count(")"))
+
+    if str.count("(") == str.count(")"):
+        print ("Parenthesis ARE balanced")
+    else:
+        print ("Parenthesis are NOT balanced")
+
+paren_matcher ("blah(balh)nahhf(())yup)))")
