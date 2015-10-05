8590c44c7c7fe8738ae1bde9662cf2772ab2a508
Wanjugu Irene <ireneemaina@gmail.com>
2015-10-05 09:47:24 +0300

    Secondy: Completed

--- secondy/secondy.py
+++ secondy/secondy.py
@@ -3,8 +3,25 @@
 # For example: if lis contains [7, 7, 12, 98, 106] the output should be 12 98.
 # The list will not be empty and will contain at least 2 numbers. It can get tricky if there's just two numbers!
 
-def Secondy(lis):
-  # code goes here
 
+def Secondy(lis):
+
+    list = sorted(lis)
+
+    #getting second number
+    second = list[0:1]
+
+    #getting second last number
+    i = len(list)
+    second_last = list [i-2:i-1]
+
+    xx = second + second_last
+    y = " ".join(str(i) for i in xx)
+    print (y)
 
 # keep this function call below here
+lis = [3,66,73,93,2,5]
+Secondy (lis)
+
+# # x = x.append(second_last)
+# # print (x)
