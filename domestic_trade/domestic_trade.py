76aa782aacc5aa777c52269c73cb7bd689945edd
Wanjugu Irene <ireneemaina@gmail.com>
2015-10-12 14:53:43 +0300

    'Domestic Trade' Problem: Completed

--- domestic_trade/domestic_trade.py
+++ domestic_trade/domestic_trade.py
@@ -34,112 +34,59 @@
 #
 # domestic_trade("DM1724")
 
-#Working with csv files
-#import csv
-import csv
-
-#opening file
-f = open('TRANS.csv')
-
-csv_f = csv.reader(f) #csv file object
-
-##########################
-#Total for particular item
-
-#filtering out everything apart from the item we need 'item_no'
-
-location = []
-item = []
-amount = []
-# item_no = raw_input('Enter the item Number: ')
-# item_no = str(item_no.upper())
-
-item_no = "DM1724"
-
-for row in csv_f:
-    if row[1] == item_no:
-        location.append(row[0])
-        item.append(row[1])
-        amount.append(int(row[2][:-4])) #removing  'KSH' from amount
 
 
-#Finding total value (KSH) of particular item sold in ALL stores
-total_KSH = 0
+def domestic_trade():
 
-for amt in amount:
-    total_KSH += amt
-print ("Total value sold for item number %s is %s" %(item_no, total_KSH))
+    import csv
+    f = open('TRANS.csv')
+    csv_f = csv.reader(f) #csv file object
 
+    ##########################
+    #Total for particular item
 
-# Finding total value (KSH) of particular item sold in ONE location
-# store_location = raw_input('Enter the store location: ')
-# store_location = store_location.lower().capitalize()
+    #filtering out everything apart from the item we need 'item_no'
+    location = []
+    item = []
+    amount = []
+    item_no = raw_input('Enter the item Number: ')
+    item_no = str(item_no.upper())
 
-store_location = "Nairobi"
-
-tot_store_amt = 0
-
-# Extracting indexes for 'store_location' stores
-loc = (i for i,x in enumerate(location) if x == store_location) #list comprehension
-for i in loc:
-    #Total Salesfrom stores
-    tot_store_amt = tot_store_amt + amount[i]
-print ("Total value sold for stores in %s is %s"  %(store_location, tot_store_amt))
-
-# Finding largest selling store
-locations = set(location)
-locations = list(locations)
-print(locations)
-print(len(locations))
-print(len(location))
-
-tot_li = []
-tot_loc_li = []
-for stuff in locations:
-    amn_li = []
-    tot = 0
-    locs = (i for i,x in enumerate(location) if x == stuff)
-    for i in locs:
-        tot = tot + amount[i]
-        amn_li = amn_li + [amount[i]]
-    print ("Total for %s is %s" %(stuff, tot))
-    print("Number of Records for %s is %s" %(stuff, len(amn_li)))
-
-    tot_li = tot_li + [tot]
-print(tot_li)
-
-# Finding greatest Number
-great = (i for i,x in enumerate(tot_li) if x == max(tot_li))
-for i in great:
-    print ("Location with greates Sales is %s" % (locations[i]))
+    for row in csv_f:
+        if row[1] == item_no:
+            location.append(row[0])
+            item.append(row[1])
+            amount.append(int(row[2][:-4])) #removing  'KSH' from amount
 
 
+    #Finding total value (KSH) of particular item sold in ALL stores
+    total_KSH = 0
 
-#
-# #---------------------------------------------------------------------------
-# #-----  Checks ----#
-# # For Nairobi
-# amn_nai_li = []
-# amt_nai = 0
-#
-# Nai = (i for i,x in enumerate(location) if x == "Nairobi")
-# for i in Nai:
-#     # print (i) #indexes for where Nairobi stores occur in list
-#     # Number of records for Nairobi
-#     amn_nai_li = amn_nai_li + [amount[i]]
-#
-#     #Total Salesfrom Nairobi stores
-#     amt_nai = amt_nai + amount[i]
-#
-#     # print (amt_nai)
-#
-#
-# # No. of records for item
-# print (len(location))
-# print (len (amn_nai_li))
-#
-#
+    for amt in amount:
+        total_KSH += amt
+
+    # Finding largest selling store
+    locations = set(location)
+    locations = list(locations)
+
+    tot_list = []
+    for stuff in locations:
+        tot = 0
+        locs = (i for i,x in enumerate(location) if x == stuff)
+        for i in locs:
+            tot = tot + amount[i]   # Calculating the totals
+        tot_list = tot_list + [tot] #Putting the totals in a list
+
+    # Finding greatest Number
+    great = (i for i,x in enumerate(tot_list) if x == max(tot_list))
+    for i in great:
+        largest = (locations[i])
+
+    results = dict([('total_KSH', total_KSH), ('largest', largest)])
+    print(results)
 
 
-#closing the file
-f.close()
+    #closing the file
+    f.close()
+
+domestic_trade()
