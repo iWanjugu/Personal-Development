5536795eea28dcbc9608a46675bcd548412793f4
Wanjugu Irene <ireneemaina@gmail.com>
2015-10-13 08:58:36 +0300

    Attempt 1: fail

--- ceaser/ceaser_cipher.py
+++ ceaser/ceaser_cipher.py
@@ -7,3 +7,45 @@
 
 # to read more about ceaser's cipher visit http://practicalcryptography.com/ciphers/caesar-cipher/ or google some more
 # happy coding :-)
+
+import string
+
+str = 'cat and dog'
+str = str.split()
+print (str)
+num = 2
+i = 0
+
+blank = " "
+alphabet = list(string.ascii_lowercase)
+alphabet = alphabet.append(blank)
+print(alphabet)
+print(len(str))
+print(len(alphabet))
+
+# Breaking down sentence
+
+
+
+
+# Breaking down words
+sent_index = []
+for j in range(0, len(str)):
+    W = (i for i,x in enumerate(alphabet) if x == str[j])
+    for i in W:
+        sent_index = sent_index + [i]
+print (sent_index)
+
+new_sent_index = [x+num for x in sent_index]
+print (new_sent_index)
+
+
+
+
+
+# for i in str:
+#     while i < len(str):
+#         print (str[i])
+
+
+
