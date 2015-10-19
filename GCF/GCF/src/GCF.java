f2427c6ac3978162711f5f67f894363e77b28b2d
Wanjugu Irene <ireneemaina@gmail.com>
2015-10-19 17:15:57 +0300

    GCF: Java version complete

--- GCF/GCF/src/GCF.java
+++ GCF/GCF/src/GCF.java
@@ -1,36 +1,31 @@
-//Using java, have the function divide(num1,num2)
-//take both parameters being passed and return the Greatest Common Factor.
-//That is, return the greatest number that evenly goes into both numbers with no remainder.
-//For example: 20 and 10 both are divisible by 1, 2, 5 and 10 so the output should be 10.
-//The range for both parameters will be from 1 to 10^3.
+// Using java, have the function divide(num1,num2)
+// take both parameters being passed and return the Greatest Common Factor.
+// That is, return the greatest number that evenly goes into both numbers with no remainder.
+// For example: 20 and 10 both are divisible by 1, 2, 5 and 10 so the output should be 10.
+// The range for both parameters will be from 1 to 10^3.
 
-
-
-import java.util.Scanner;
-import java.lang.Math;
+//import java.util.Scanner;
 
 public class GCF {
-	public static int divide(int num1, int num2){
-		int i = 0;
+	private static int divide(int num1, int num2)  {
+		// TODO Auto-generated method stub
+		int c = 0, cf = 0, i = 1;
+		c = Math.min(num1, num2);
 
-		for (i=0;i<=Math.min(num1,num2);i++){
-			if ((num1%i==0) == (num2%i==0)){
-				System.out.print (i);
-			}
-		}
-		return i;
+		for (i=1; i <= c+1; i++)
+    	{
+    		if (num1%i==0 && num2%i==0 )
+    		{
+              cf = i;
+    		}
+    	}
+    	return cf;
 
 	}
-	public static void main(String[] args) {
-		Scanner num = new Scanner(System.in);
-		int num1 = 0, num2 = 0;
 
-		System.out.println ("Enter first number: ");
-		num1 = num.nextInt();
-		System.out.println ("Enter Second number: ");
-		num2 = num.nextInt();
-
-	    System.out.print(divide(num1, num2));
-	}
+    public static void main (String args []){
+    	int cf = divide(27, 36);
+    	System.out.println (cf);
+    }
 }
 
