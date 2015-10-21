a9bafbb540aca050dd5bb5fa50efdcfc638b8693
Wanjugu Irene <ireneemaina@gmail.com>
2015-10-21 14:04:27 +0300

    Key Press:  WITHOUT HARD CODING - Complete!

--- key_press/key_press.py
+++ key_press/key_press.py
@@ -43,80 +43,87 @@ from numpy import array
 import string
 import numpy as np
 
-# def presses(str):
-####################################
-###### Keypad values
+def presses():
+    ####################################
+    ###### Keypad values
+
+    ### Create a matrix of values just like the keypad.
+    # Index value will be the  number of times tobe pressed
+
+    a = list(string.lowercase)
+    matrix = []
+
+    #creating the one digit Matrix
+    ones = ('1','*', '0', '#')
+    matrix1 = np.zeros((1,1))
+
+    for x in ones:
+        matrix1 = np.concatenate((matrix1, np.matrix([x])))
+
+    #creating the 4 digit matrix
+    i = range(0, 27)
+    a_3 = a[:15]
+    matrix4 = np.zeros((0, 4))
+
+    for i, item in enumerate(a_3):
+        if (i%3==0):
+            matrix4 = np.concatenate((matrix4 , [a[i:i+3] + [(i/3)+2]]))
+
+    #creating the 5 digit matrix
+    matrix5 = array ((a[15:19] + [7],\
+                  a[22:]+[9]))
 
 
-### Create a matrix of values just like the keypad.
-# Index value will be the  number of times tobe pressed
-
-a = list(string.lowercase)
-matrix = []
-
-#creating the one digit entities
-ones = ('1','*', '0', '#')
-matrix1 = np.zeros((1,1))
-
-for x in ones:
-    matrix1 = np.concatenate((matrix1, np.matrix([x])))
-print (matrix1)
-
-#creating the three digit entities
-i = range(0, 27)
-a_3 = a[:15]
-matrix4 = np.zeros((0, 4))
-
-for i, item in enumerate(a_3):
-    if (i%3==0):
-        matrix4 = np.concatenate((matrix4 , [a[i:i+3] + [(i/3)+2]]))
-
-print (matrix4)
-
-#creating the four digit entities
-
-# z = range(15, 27)
-matrix5 = array ((a[15:19] + [7],\
-              a[22:]+[9]))
-print matrix5
-
-# # matrix1
-#  [['1']
-#  ['*']
-#  ['0']
-#  ['#']]
-#
-# # matrix4
-# [['a' 'b' 'c' '2']
-#  ['d' 'e' 'f' '3']
-#  ['g' 'h' 'i' '4']
-#  ['j' 'k' 'l' '5']
-#  ['m' 'n' 'o' '6']]
-#
-# # matrix5
-# [['p' 'q' 'r' 's' '7']
-#  ['w' 'x' 'y' 'z' '9']]
+    # # matrix1
+    #  [['1']
+    #  ['*']
+    #  ['0']
+    #  ['#']]
+    #
+    # # matrix4
+    # [['a' 'b' 'c' '2']
+    #  ['d' 'e' 'f' '3']
+    #  ['g' 'h' 'i' '4']
+    #  ['j' 'k' 'l' '5']
+    #  ['m' 'n' 'o' '6']]
+    #
+    # # matrix5
+    # [['p' 'q' 'r' 's' '7']
+    #  ['w' 'x' 'y' 'z' '9']]
 
 
-# finding index of a value in matrix
-val = 'a'
-val_index = np.where(matrix4 == val)
-print (val_index) #index of value you're looking for
+    #-----------------------------------------------------------#
+    # The calculation #
 
-#the y (column) index i,e the number of times the keypad will be pressed to get the desired letter/ number
+    matrices = [matrix1, matrix4, matrix5]
 
-key_press = int (val_index[1]) + 1
-print (key_press)
+    input = raw_input('Input a sentence: ')
+
+    # Change input to lowercase and string
+    input = str(input.lower())
+
+    tot_key_press = 0
+
+    for letter in input:
+
+        for mat in matrices:
+
+            val_index = np.where(mat == letter)
+            empty_index = np.shape(val_index) [1]
+
+            if empty_index == 0:
+                key_press_num = 0
+
+            else:
+                key_press_num = int (val_index[1]) + 1
+                tot_key_press = tot_key_press + key_press_num
+
+    spaces = input.count(" ")
+    tot_key_press = tot_key_press + spaces
+
+    print ("Total number of key presses is %s " %(tot_key_press))
+
+presses ()
 
 
 
-
-#
-# # Change to lowercase and string
-# input = "BoY"
-#
-# input = str(input.lower())
-# print (input)
-# #
-# for letter in input:
-#
