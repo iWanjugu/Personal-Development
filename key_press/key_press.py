7c9aaf985f0ef305fc779e23d5282016bf29897c
Wanjugu Irene <ireneemaina@gmail.com>
2015-10-21 12:46:31 +0300

    Key Press: Without hard coding - Matrices created for keypress patterns

--- key_press/key_press.py
+++ key_press/key_press.py
@@ -27,7 +27,8 @@
 
 # Note: the 0 key handles spaces and outputs 0 when tapped twice.
 
-# Write a function that can calculate the amount of button presses required for any phrase. Except for spaces, punctuation can be ignored. Your function should accept both uppercase and lowercase letters and treat them the same.
+# Write a function that can calculate the amount of button presses required for any phrase. Except for spaces, punctuation can be ignored.
+# Your function should accept both uppercase and lowercase letters and treat them the same.
 
 # Examples:
 
@@ -38,6 +39,60 @@
 
 # Bonus: Try to avoid hard-coding the number of button presses for each letter!
 
+from numpy import array
+import string
+import numpy as np
 
-def presses(str):
-  # Your Code Here!
+# def presses(str):
+####################################
+###### Keypad values
+
+
+### Create a matrix of values just like the keypad.
+# Index value will be the  number of times tobe pressed
+
+a = list(string.lowercase)
+matrix = []
+
+#creating the one digit entities
+ones = ('1','*', '0', '#')
+matrix1 = np.zeros((1,1))
+
+for x in ones:
+    matrix1 = np.concatenate((matrix1, np.matrix([x])))
+print (matrix1)
+
+#creating the three digit entities
+i = range(0, 27)
+a_3 = a[:15]
+matrix4 = np.zeros((0, 4))
+
+for i, item in enumerate(a_3):
+    if (i%3==0):
+        matrix4 = np.concatenate((matrix4 , [a[i:i+3] + [(i/3)+2]]))
+
+print (matrix4)
+
+#creating the four digit entities
+
+# z = range(15, 27)
+matrix5 = array ((a[15:19] + [7],\
+              a[22:]+[9]))
+print matrix5
+
+# # matrix1
+#  [['1']
+#  ['*']
+#  ['0']
+#  ['#']]
+#
+# # matrix4
+# [['a' 'b' 'c' '2']
+#  ['d' 'e' 'f' '3']
+#  ['g' 'h' 'i' '4']
+#  ['j' 'k' 'l' '5']
+#  ['m' 'n' 'o' '6']]
+#
+# # matrix5
+# [['p' 'q' 'r' 's' '7']
+#  ['w' 'x' 'y' 'z' '9']]
