f226562a345a022b6059b59b8d8581fca00e172d
Wanjugu Irene <ireneemaina@gmail.com>
2015-10-23 17:41:29 +0300

    Remove Duplicate:  attempt, not complete

--- remove_duplicate/remove_duplicate.py
+++ remove_duplicate/remove_duplicate.py
@@ -1,8 +1,27 @@
- # Using PYTHON solve the problem below:
 
- # Remove duplicate characters in a given string keeping only the first occurrences.
- # For example, if the input is ‘tree traversal’ the output will be "tre avsl".
+# def removeDuplicates(string):
+# Your code here!
 
-def removeDuplicates(string):
- 	# Your code here!
+str = "bad boy"
+str_ = str.split()
+print str_
+q = range(len(str))
+con_sent = []
 
+for num in q:
+    print(str[num])
+    con_sent = con_sent.append(str(num))
+
+# for letter in str:
+#     #getting
+#     ind_pos = (i for i, x in enumerate (str) if x == letter)
+#     for z in ind_pos:
+#         lett_pos = []
+#         lett_pos = lett_pos + [z]
+#         print (z)
+#         print (lett_pos)
+
+#
+# for i in str:
+#     print (i)
+#     print(str.count(i))
